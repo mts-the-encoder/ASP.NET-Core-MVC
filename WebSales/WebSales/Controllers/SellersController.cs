a3495409319082475aeb5d7e9bec0d243cefec2b
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-10-20 11:09:09 -0300

    Create form and Post method

--- WebSales/WebSales/Controllers/SellersController.cs
+++ WebSales/WebSales/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebSales.Models.Entities;
 using WebSales.Services;
 
 namespace WebSales.Controllers
@@ -16,6 +17,19 @@ namespace WebSales.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View(nameof(Create));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
