f015f57a6546015bf368f0b03646e7837b19d3bd
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-10-20 11:18:21 -0300

    Foreign key in Seller, not null

--- WebSales/WebSales/Migrations/20221020141311_DeparmentForeignKey.Designer.cs
+++ WebSales/WebSales/Migrations/20221020141311_DeparmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebSales.Data;
 
 namespace WebSales.Migrations
 {
     [DbContext(typeof(WebSalesContext))]
-    partial class WebSalesContextModelSnapshot : ModelSnapshot
+    [Migration("20221020141311_DeparmentForeignKey")]
+    partial class DeparmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- WebSales/WebSales/Models/Entities/Seller.cs
+++ WebSales/WebSales/Models/Entities/Seller.cs
@@ -13,8 +13,8 @@ namespace WebSales.Models.Entities
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
-
         public Seller()
         {
         }
