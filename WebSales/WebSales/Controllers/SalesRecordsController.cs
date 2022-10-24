e66d7f1befc50d1cea615adb80600d996fcc6ae3
mts-the-encoder <matheus.nsantos@bandtec.com.br>
2022-10-24 02:55:47 -0300

    implemeting simple search

--- WebSales/WebSales/Controllers/SalesRecordsController.cs
+++ WebSales/WebSales/Controllers/SalesRecordsController.cs
@@ -1,16 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using WebSales.Services;
 
 namespace WebSales.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        private readonly SalesRecordsService _salesRecordsService;
+
+        public SalesRecordsController(SalesRecordsService salesRecordsService)
+        {
+            _salesRecordsService = salesRecordsService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult SimpleSearch()
+        public IActionResult SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            minDate ??= new DateTime(DateTime.Now.Year, 1, 1);
+
+            maxDate ??= DateTime.Now;
+
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var res = _salesRecordsService.FindByDate(minDate, maxDate);
+            return View(res);
         }
         public IActionResult GroupingSearch()
         {
---
+++ WebSales/WebSales/Services/SalesRecordsService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using WebSales.Data;
+using WebSales.Models.Entities;
+using System.Linq;
+
+
+namespace WebSales.Services
+{
+    public class SalesRecordsService
+    {
+        private readonly WebSalesContext _context;
+
+        public SalesRecordsService(WebSalesContext context)
+        {
+            _context = context;
+        }
+
+        public List<SalesRecord> FindByDate(DateTime? minDate, DateTime? maxDate)
+        {
+            var res = from obj in _context.SalesRecords select obj;
+
+            if (minDate.HasValue)
+            {
+                res = res.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                res = res.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return res
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToList();
+        }
+
+    }
+}
